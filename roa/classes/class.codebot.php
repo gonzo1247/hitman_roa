4d2a0ac2f94f99d5860fb8c411b98f4567037f41
sevi <gonzo1247@yahoo.de>
2015-08-14 16:15:28 +0200

    Add basics for codebot class. that needs some more function and so.

---
+++ roa/classes/class.codebot.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by Severin Weitz [gonzo1247@yahoo.de]
+ * Date: 2015/08/14
+ * Time: 16:11
+ */
+class codebot {
+
+	public static function addcode($username = "", $itemid = "", $itemcode = "", $titleid = "", $achievid = "", $charguid = "" ) {
+
+	}
+
+}
