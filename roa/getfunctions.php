6ef3cafe6f88ab32d077d857a80242a18875a0ae
sevi <gonzo1247@yahoo.de>
2015-10-30 20:01:56 +0100

    Update

--- roa/getfunctions.php
+++ roa/getfunctions.php
@@ -12,7 +12,27 @@ function getfunctionOutput() {
 		case 'control':
 			//todo
 		case 'codebot':
-			return "Codebot";
+			return "<form action=\"code_generatet.php\" method=\"get\">
+    <p>1000 Punkte f?r einen Eigenen Forenavatar Eintauschen </p><p>
+    <input type=\"submit\" value=\"Einloesen:\" />
+    </form>
+		<form action=\"code_generatet.php\" method=\"get\">
+    <p>2500 Punkte f?r eine Charakterwiederherstellung Eintauschen, sofern m?glich. </p><p>
+    <input type=\"submit\" value=\"Einloesen:\" />
+    </form>
+	<form action=\"code_generatet.php\" method=\"get\">
+    <p>1000 Punkte f?r einen Charaktertransfer Eintauschen </p><p>
+    <input type=\"submit\" value=\"Einloesen:\" />
+    </form>
+	<form action=\"code_generatet.php\" method=\"get\">
+    <p>500 Punkte gegen eine Charakter Geschlechtsumwandlung Eintauschen </p><p>
+    <input type=\"submit\" value=\"Einloesen:\" />
+    </form>
+	<form action=\"code_generatet.php\" method=\"get\">
+    <p>6000 Punkte gegen eine \"BigMac\" RoA-Tasche ( 36 Pl?tze )
+( Achtung: Seelengebunden und einzigartig Anlegbar! )  </p><p>
+    <input type=\"submit\" value=\"Einloesen:\" />
+    </form>";
 		default:
 			return "Unbekannter Modus";
 	}
